7219983f3e330f81a84bfe51baa71794a3857718
Leafqun <leafqun@yeah.net>
2019-10-21 09:41:49 +0800

    1021

---
+++ src/main/java/com/wuye/manage/wuye/WuyeApplication.java
@@ -0,0 +1,13 @@
+package com.wuye.manage.wuye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wuye/manage/wuye/WuyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuye.manage.wuye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
